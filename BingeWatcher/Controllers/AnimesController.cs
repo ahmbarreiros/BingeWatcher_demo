87c7545697a7d3ae457ea18840eafc64b13f1f56
Antonio <ahmbarreiros.cs@gmail.com>
2023-08-23 16:51:14 -0300

    Adicionar arquivos de projeto.

---
+++ BingeWatcher/Controllers/AnimesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BingeWatcher.Controllers
+{
+    public class AnimesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BingeWatcher/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BingeWatcher.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
