19989564f9ed054415d15bd4eef8111104b5660f
Antonio <ahmbarreiros.cs@gmail.com>
2023-08-23 18:02:50 -0300

    task to call webapi created

--- BingeWatcher/Controllers/AnimeController.cs
+++ BingeWatcher/Controllers/AnimeController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BingeWatcher.Data;
+using Microsoft.AspNetCore.Mvc;
 
 
 namespace BingeWatcher.Controllers
 {
     public class AnimeController : Controller
     {
+        public readonly AppDbContext _db;
+        public AnimeController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> SyncWithAPI()
+        {
+            HttpClient client = new HttpClient();
+            const string URL = "https://api.myanimelist.net/v2";
+            const string query = "/anime/ranking?ranking_type=all";
+            client.DefaultRequestHeaders.Add("X-MAL-CLIENT-ID", "123");
+            var response = await client.GetAsync((URL+query));
+            return RedirectToAction("index");
+
+        }
+
+
         public IActionResult Index()
         {
+
             return View();
         }
     }
